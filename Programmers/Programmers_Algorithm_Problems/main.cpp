2db13c29228fa7b7641b0accffa76741566f29d6
ohseil <sioh0612@gmail.com>
2022-06-01 12:44:55 +0900

    '소수 찾기' 문제 풀이 추가

--- Programmers/Programmers_Algorithm_Problems/main.cpp
+++ Programmers/Programmers_Algorithm_Problems/main.cpp
@@ -9,7 +9,8 @@ int main() {
 	//전화번호_목록::service();
 	//프린터::service();
 	//디스크컨트롤러::service();
-	가장_큰_수::service();
+	//가장_큰_수::service();
+	소수_찾기::service();
 
 	return 0;
 }
---
+++ Programmers/Programmers_Algorithm_Problems/소수_찾기.h
@@ -0,0 +1,3 @@
+#pragma once
+
+namespace 소수_찾기 { void service(); }
