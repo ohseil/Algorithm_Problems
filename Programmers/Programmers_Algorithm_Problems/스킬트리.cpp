3d3cf4c97b9e28f21cd0a018101d4c58cf107f7d
ohseil <sioh0612@gmail.com>
2022-10-29 04:46:57 +0900

    '스킬트리' 문제 풀이 수정

--- Programmers/Programmers_Algorithm_Problems/스킬트리.cpp
+++ Programmers/Programmers_Algorithm_Problems/스킬트리.cpp
@@ -1,33 +1,34 @@
 #include "스킬트리.h"
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <unordered_set>
 
 using namespace std;
 
 스킬트리_BEGIN
 
+#define HashMap unordered_map
 #define HashSet unordered_set
 
-#define NOT_IN_PRECEDING_SKILL_ORDER -1
+void setPrecedingSkillMap(string& skill, HashMap<char, char>& precedingSkillMap) {
+    for (int i = 1; i < skill.length(); i++)
+        precedingSkillMap[skill[i]] = skill[i - 1];
+}
 
-bool canLearnSkillTree(string& skillTree, string& skills) {
+bool canLearnSkillTree(string& skillTree, HashMap<char, char>& precedingSkillMap) {
 
-    HashSet<char> learnedSkills;
+    HashSet<char> learnedSkillSet;
 
     for (char skill : skillTree) {
 
-        int skillIdx = skills.find(skill);
-
-        if (skillIdx != NOT_IN_PRECEDING_SKILL_ORDER) {
-            for (int i = 0; i < skillIdx; i++) {
-                char needSkill = skills[i];
-                if (learnedSkills.count(needSkill) == 0)
-                    return false;
-            }
+        if (precedingSkillMap.count(skill) > 0) {
+            char precedingSkill = precedingSkillMap[skill];
+            if (learnedSkillSet.find(precedingSkill) == learnedSkillSet.end())
+                return false;
         }
 
-        learnedSkills.insert(skill);
+        learnedSkillSet.insert(skill);
     }
 
     return true;
@@ -39,13 +40,21 @@ int solution(string skill, vector<string> skill_trees) {
     // 배울 수 있는 조건은
     // 1. 선행 스킬 순서에 포함되지 않는 경우
     // 2. 배워야할 스킬의 선행 스킬들을 모두 배운 경우
+    //    ( 결국, 바로 앞의 선행 스킬을 배웠는지만 확인하면 된다. 바로 앞의 스킬을 배웠다는 것은 이미 다 배웠다는 것이므로. )
 
-    // 스킬을 배웠다면 hash set을 사용해서 기록하고 다음에 확인할때 사용한다.
+    // 선행 스킬 순서 문자열을 순회해서 hash set에 각 스킬마다 선행해야 하는 바로 앞의 스킬 정보를 저장한다.
+    // 배워야 할 스킬이 선행 스킬 순서에 포함된다면,
+    // 선행 스킬 정보를 확인하고 해당 스킬을 배웠는지 확인한다.
+    // 배운 스킬 정보는 별개로 hash set을 사용해서 기록해 나간다.
 
     int answer = 0;
     
+    HashMap<char, char> precedingSkillMap;
+
+    setPrecedingSkillMap(skill, precedingSkillMap);
+
     for (string skillTree : skill_trees) {
-        if (canLearnSkillTree(skillTree, skill) == true)
+        if (canLearnSkillTree(skillTree, precedingSkillMap) == true)
             answer++;
     }
     
